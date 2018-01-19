fe7967c883860cac2232d7c28606cd0354395dd0
julianpstys <julianstys@me.com>
2018-01-18 18:11:42 -0700

    Committing lonely tweiter

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -0,0 +1,11 @@
+package ca.ualberta.cs.lonelytwitter;
+import java.util.Date;
+
+/**
+ * Created by jstys on 1/18/18.
+ */
+
+public interface Tweetable {
+    String getMessage();
+    Date getDate();
+}
